9051eebb990a8a4f8ecdb132bd4bddc897f58717
Leanid Shutau <leanidshutau@gmail.com>
2020-11-21 18:12:49 +0300

    integrate fe with be for task-5 (#5)

--- src/constants/apiPaths.ts
+++ src/constants/apiPaths.ts
@@ -2,7 +2,7 @@
 const API_PATHS = {
   product: 'https://g5sd3df7lf.execute-api.eu-west-1.amazonaws.com/dev',
   order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-  import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+  import: 'https://xym1o41yw6.execute-api.eu-west-1.amazonaws.com/dev',
   bff: 'https://.execute-api.eu-west-1.amazonaws.com/dev'
 };
 
